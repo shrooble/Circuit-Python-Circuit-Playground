427862578cb0360e74fac472dd9524a76f23e2d2
shrooble <113949103+shrooble@users.noreply.github.com>
2024-01-17 08:31:35 -0800

    Uploaded Code.py

---
+++ Code.py
@@ -0,0 +1,10 @@
+print("hello")
+msg = input()
+if msg == "hello" or msg == "hi" or msg == "bye":
+  print("bye")
+  exit()
+if msg == "run math" or msg == "math":
+  print("insert an equation")
+  math = input()
+  print(eval(math))
+  
