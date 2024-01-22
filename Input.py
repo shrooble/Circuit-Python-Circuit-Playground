cc133ac4f0d64d8ae375271f7e23e065674e2148
shrooble <113949103+shrooble@users.noreply.github.com>
2024-01-22 10:23:57 -0800

    Update Input.py

--- Input.py
+++ Input.py
@@ -1,10 +1,19 @@
 print("hello")
-msg = input()
-if msg == "hello" or msg == "hi" or msg == "bye":
-  print("bye")
-  exit()
-if msg == "run math" or msg == "math":
-  print("insert an equation")
-  math = input()
-  print(eval(math))
-  
+greeting_demo = 1
+math_demo = 1
+
+def greeting
+def math
+
+while True:
+  if greeting_demo:
+    msg = input()
+    if msg == "hello" or msg == "hi" or msg == "bye":
+      print("bye")
+      exit()
+  if math_demo:
+   if msg == "run math" or msg == "math":
+      print("insert an equation")
+      x = input()
+      print(eval(x))
+      
