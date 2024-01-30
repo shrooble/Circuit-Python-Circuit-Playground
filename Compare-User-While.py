feac3c8286057f5ccdfed4e9df740060d1521660
shrooble <113949103+shrooble@users.noreply.github.com>
2024-01-30 09:38:59 -0800

    Update Compare-User-While.py
    update

--- Compare-User-While.py
+++ Compare-User-While.py
@@ -7,20 +7,21 @@ def main():
   while y:
     try:
       user = int(input("Enter a number between 0-9: "))
-      if user > 9 or user < 9: 
-        continue
     except ValueError:
+      print("Error")
       continue
     else:
+      if user > 9 or user < 0: 
+        continue
       x = 0
       print("Starting While Loop - Comparing User and Count Variable")
       while x != 10:
         print(x)
-        x += 1
         if x == user:
           print("The User variable is equal to the count variable: ", x)
           print("user = ", user)
           print("x = ", x)
+        x += 1
       print("Ending While Loop")
       y = False
 main()
