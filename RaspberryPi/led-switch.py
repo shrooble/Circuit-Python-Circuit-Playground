3fa0bbf870fccd021bec1bce6be8b0b4f2b6ecae
shrooble <113949103+shrooble@users.noreply.github.com>
2024-03-08 08:15:08 -0800

    Create led-switch.py

---
+++ RaspberryPi/led-switch.py
@@ -0,0 +1,23 @@
+import RPi.GPIO as GPIO
+from time import sleep
+
+GPIO.setmode(GPIO.BOARD)
+GPIO.setwarnings(False)
+GPIO.setup(5, GPIO.OUT)
+GPIO.setup(40, GPIO.IN)
+
+led = 5
+switch = 40
+switchState = 0
+count = 0
+
+while True:
+    print(count)
+    GPIO.output(led, GPIO.input(switch))
+    switchState = GPIO.input(switch)
+    if switchState != 0:
+       count += 1
+       while switchState != 0:
+           switchState = GPIO.input(switch)
+           if switchState != 1:
+               continue
